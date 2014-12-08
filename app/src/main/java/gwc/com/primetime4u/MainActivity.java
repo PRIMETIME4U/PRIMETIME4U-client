d8adbea880100d80227d582db3bae003c6953e35
giovannicolonna <gc240790@gmail.com>
2014-12-08 17:41:07 +0100

    Visualizzazione lista parsata nella home. Operazioni di rete ok Uso libreria Volley per richieste JSON

--- app/src/main/java/gwc/com/primetime4u/MainActivity.java
+++ app/src/main/java/gwc/com/primetime4u/MainActivity.java
@@ -1,5 +1,6 @@
 package gwc.com.primetime4u;
 
+import android.app.ProgressDialog;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -7,16 +8,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ListView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import primetime4u.adapter.CustomListAdapter;
+import primetime4u.app.AppController;
+import primetime4u.model.Movie;
 
 
 public class MainActivity extends FragmentActivity {
 
+    // Log tag
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String url = "http://api.androidhive.info/json/movies.json";
+    private ProgressDialog pDialog;
+    private List<Movie> movieList = new ArrayList<Movie>();
+    private ListView listView;
+    private CustomListAdapter adapter;
 
 
     //VERSIONE CONDIVISA SU GITHUB, 07.12.2014 ore 13.51
@@ -32,7 +58,76 @@ public class MainActivity extends FragmentActivity {
             ft.add(R.id.container, button);
             ft.addToBackStack(null);
             ft.commit();
+
+
+
         }
+
+        listView = (ListView) findViewById(R.id.listView);
+        adapter = new CustomListAdapter(this, movieList);
+        listView.setAdapter(adapter);
+
+        pDialog = new ProgressDialog(this);
+        // Showing progress dialog before making http request
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        // changing action bar color
+        //getActionBar().setBackgroundDrawable(
+        //       new ColorDrawable(Color.parseColor("#1b1b1b")));
+
+        // Creating volley request obj
+        JsonArrayRequest movieReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        hidePDialog();
+
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                Movie movie = new Movie();
+                                movie.setTitle(obj.getString("title"));
+                                movie.setThumbnailUrl(obj.getString("image"));
+                                movie.setRating(((Number) obj.get("rating"))
+                                        .doubleValue());
+                                movie.setYear(obj.getInt("releaseYear"));
+
+                                // Genre is json array
+                                JSONArray genreArry = obj.getJSONArray("genre");
+                                ArrayList<String> genre = new ArrayList<String>();
+                                for (int j = 0; j < genreArry.length(); j++) {
+                                    genre.add((String) genreArry.get(j));
+                                }
+                                movie.setGenre(genre);
+
+                                // adding movie to movies array
+                                movieList.add(movie);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hidePDialog();
+
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(movieReq);
 
         //CHIEDI AL SERVER LISTA FILM VISTI -> ACTIVITY LISTA FILM
 
@@ -45,9 +140,15 @@ public class MainActivity extends FragmentActivity {
 
 
 
+
     }
 
-
+    private void hidePDialog() {
+        if (pDialog != null) {
+            pDialog.dismiss();
+            pDialog = null;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
